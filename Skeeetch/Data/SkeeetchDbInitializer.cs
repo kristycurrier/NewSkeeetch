169f0d29fd5b3575e0b8c01c973d44c5c6b63108
Kristy Currier <kristy.currier@gmail.com>
2019-03-09 10:22:42 -0500

    New repo

---
+++ Skeeetch/Data/SkeeetchDbInitializer.cs
@@ -0,0 +1,24 @@
+﻿using Skeeetch.Models;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace Skeeetch.Data
+{
+    public class SkeeetchDbInitializer : CreateDatabaseIfNotExists<SkeeetchContext>
+    {
+        protected override void Seed(SkeeetchContext context)
+        {
+            var categories = new List<Category>
+            {
+                new Category{ID = 1, DisplayTerm = "Drinks", SearchTerm = "booze"},
+                new Category{ID = 2, DisplayTerm = "Food", SearchTerm = "hungry"},
+                new Category{ID = 3, DisplayTerm = "Art", SearchTerm = "creative"},
+                new Category{ID = 4, DisplayTerm = "Chilling", SearchTerm = "relaxing"},
+                new Category{ID = 5, DisplayTerm = "Active", SearchTerm = "excitement"}
+            };
+
+            categories.ForEach(k => context.Categories.Add(k));
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Skeeetch/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Skeeetch.Models
+{
+    public class Category
+    {
+        public int ID { get; set; }
+        public string DisplayTerm { get; set; }
+        public string SearchTerm { get; set; }
+
+        public virtual ICollection<Category> Categories { get; set; }
+    }
+}
